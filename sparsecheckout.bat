c472fe02abb4f4454fc8cd5773d4b44b052e6d2f
mingburnu <program.developer@hotmail.com>
2017-05-02 11:46:44 +0800

    Update sparsecheckout.bat

--- sparsecheckout.bat
+++ sparsecheckout.bat
@@ -3,11 +3,12 @@
 SET /P remote=remote url :
 SET /P local=local directory :
 SET /P target=target/* :
+SET /P branch=branch name :
 
 git clone -n %remote% %local%
 cd %local%
 git config core.sparsecheckout true
 echo %target% >> .git/info/sparse-checkout
-git checkout master
+git checkout %branch%
 
 pause
